81e6a99fb350b2e7a73ebfdfb34b3fd348e38029
Dionatan <dionatan.1751@gmail.com>
2023-01-06 16:56:45 -0300

    integrando api ao front-end

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const connection = require("./database")
+const cors = require("cors")
 
 const Games = require("./gameDB")
+
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
