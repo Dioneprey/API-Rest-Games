66c7add7096af26c4011d8f2a191bd8a5e618a00
Dionatan <dionatan.1751@gmail.com>
2023-01-07 11:30:35 -0300

    hateoas

--- index.js
+++ index.js
@@ -51,9 +51,34 @@ connection
 
 app.get("/games", auth, (req, res) => {
     
+    var HATEOAS = [
+        
+        {
+            href: "http://localhost:8080/game/:id",
+            method: "GET",
+            rel: "get_game"
+        },
+        {
+            href: "http://localhost:8080/auth",
+            method: "POST",
+            rel: "login"
+        },
+        {
+            href: "http://localhost:8080/game/:id",
+            method: "PUT",
+            rel: "update"
+        },
+        {
+            href: "http://localhost:8080/game/:id",
+            method: "DELETE",
+            rel: "delete_game"
+        }
+    ]
+
     Games.findAll().then(game => {  
-        res.status(200).json(game)        
+        res.status(200).json({games: game, _links: HATEOAS})        
     })
+
 })
 
 app.get("/game/:id", auth, (req, res) => {
@@ -62,9 +87,33 @@ app.get("/game/:id", auth, (req, res) => {
     } else {
         var id = parseInt(req.params.id)
 
+        var HATEOAS = [
+        
+            {
+                href: "http://localhost:8080/game/"+id,
+                method: "PUT",
+                rel: "update"
+            },
+            {
+                href: "http://localhost:8080/game/"+id,
+                method: "DELETE",
+                rel: "delete_game"
+            },
+            {
+                href: "http://localhost:8080/game/"+id,
+                method: "GET",
+                rel: "get_game"
+            },
+            {
+                href: "http://localhost:8080/games",
+                method: "GET",
+                rel: "get_all_games"
+            }        
+        ]
+
         Games.findByPk(id).then(game => {
             res.status(200)
-            res.json(game)            
+            res.json({games: game, _link: HATEOAS})            
         })
     }
 
